6438b6f32317cd241716b335730f5a89908f4e55
Justin Miranda <jcm62@columbia.edu>
2019-10-08 16:04:20 -0500

    Fixed isssue with footer

--- grails-app/controllers/org/pih/warehouse/user/DashboardController.groovy
+++ grails-app/controllers/org/pih/warehouse/user/DashboardController.groovy
@@ -182,7 +182,7 @@ class DashboardController {
     }
 
     def footer = {
-        render(template: "/common/footer", model: [gitProperties:gitProperties, buildProperties: buildProperties, infoProperties: infoProperties])
+        render(template: "/common/footer", model: [gitProperties:gitProperties])
     }
 
     //@Cacheable("megamenuCache")
